257f6f3d29f9b1985a68a50900f9754a06bac889
agnesnora <agnesnora@gmail.com>
2026-03-16 14:32:14 +0100

    setting up global and landingpage with middleware

---
+++ src/api/landing-page/middlewares/landing-page-populate.ts
@@ -0,0 +1,31 @@
+/**
+ * `landing-page-populate` middleware
+ */
+
+import type { Core } from "@strapi/strapi";
+
+const populate = {
+  populate: {
+    blocks: {
+      on: {
+        "blocks.hero": {
+          populate: {
+            links: true,
+            image: {
+              fields: ["alternativeText", "url"],
+            },
+          },
+        },
+      },
+    },
+  },
+};
+export default (config, { strapi }: { strapi: Core.Strapi }) => {
+  // Add your own logic here.
+  return async (ctx, next) => {
+    strapi.log.info("In landing-page-populate middleware.");
+    console.dir(ctx.query, { depth: null });
+    ctx.query.populate = populate;
+    await next();
+  };
+};
---
+++ types/generated/components.d.ts
@@ -0,0 +1,91 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface BlocksHero extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_heroes';
+  info: {
+    displayName: 'Hero';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images' | 'files'>;
+    links: Schema.Attribute.Component<'shared.link', true>;
+    text: Schema.Attribute.RichText;
+  };
+}
+
+export interface LayoutBanner extends Struct.ComponentSchema {
+  collectionName: 'components_layout_banners';
+  info: {
+    displayName: 'Banner';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    isVisible: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    link: Schema.Attribute.Component<'shared.link', false>;
+  };
+}
+
+export interface LayoutFooter extends Struct.ComponentSchema {
+  collectionName: 'components_layout_footers';
+  info: {
+    displayName: 'Footer';
+  };
+  attributes: {
+    logo: Schema.Attribute.Component<'shared.logo-link', false>;
+    navItems: Schema.Attribute.Component<'shared.link', true>;
+    socialLinks: Schema.Attribute.Component<'shared.logo-link', true>;
+    text: Schema.Attribute.Text;
+  };
+}
+
+export interface LayoutHeader extends Struct.ComponentSchema {
+  collectionName: 'components_layout_headers';
+  info: {
+    displayName: 'Header';
+  };
+  attributes: {
+    cta: Schema.Attribute.Component<'shared.link', false>;
+    logo: Schema.Attribute.Component<'shared.logo-link', false>;
+    navItems: Schema.Attribute.Component<'shared.link', true>;
+  };
+}
+
+export interface SharedLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    isButtonLink: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String;
+    type: Schema.Attribute.Enumeration<['PRIMARY', 'SECONDARY']>;
+  };
+}
+
+export interface SharedLogoLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_logo_links';
+  info: {
+    displayName: 'Logo Link';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images' | 'files'>;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'blocks.hero': BlocksHero;
+      'layout.banner': LayoutBanner;
+      'layout.footer': LayoutFooter;
+      'layout.header': LayoutHeader;
+      'shared.link': SharedLink;
+      'shared.logo-link': SharedLogoLink;
+    }
+  }
+}
