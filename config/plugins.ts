bd9e30b0e5ef735ed75381ff4d17a6f6ff0f622d
agnesnora <agnesnora@gmail.com>
2026-03-25 11:59:40 +0100

    adding new plants collection type

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,19 @@
+/**
+ * Plugins configuration
+ */
+
 import type { Core } from "@strapi/strapi";
 
 const config = ({
   env,
-}: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+}: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  i18n: {
+    enabled: true,
+    config: {
+      defaultLocale: "hu",
+      locales: ["hu", "en"],
+    },
+  },
+});
 
 export default config;
